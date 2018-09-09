3f88ade3d802c681ba1176a032c7e49d96aea5fc
bomb <qingqi_lei@icloud.com>
2018-09-09 13:13:59 +0800

    test

---
+++ API/src/Introspection/BeanTest.java
@@ -0,0 +1,8 @@
+package Introspection;
+
+public class BeanTest {
+    public static void main(String[] args){
+        Config bean = (Config) BeanFactory.getBean("bean.name");
+        System.out.println(bean);
+    }
+}
