9da88d97b2479f155c1a74b41ae7d2a45a7e7ce7
bomb <qingqi_lei@icloud.com>
2018-09-09 10:46:14 +0800

    Stream Usage

--- API/src/container/StreamDemo.java
+++ API/src/container/StreamDemo.java
@@ -6,5 +6,6 @@ public class StreamDemo {
     public static void main(String[] args){
         Stream<String> stream = Stream.of("good","good","study","day","day","up");
         stream.forEach((str)-> System.out.print(str+" "));
+
     }
 }
