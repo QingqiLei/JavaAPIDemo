c8e02b3bbc0b3e38aa0efb80737cdfa74d659e26
bomb <qingqi_lei@icloud.com>
2018-09-09 10:41:21 +0800

    Collections Usage

--- API/src/container/CollectionDemo.java
+++ API/src/container/CollectionDemo.java
@@ -2,6 +2,7 @@ package container;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,9 +13,25 @@ public class CollectionDemo {
         list.add(1);
         list.add(4);
         list.add(3);
-        Collections.reverse(list);
-        System.out.println(list);
-
+        Collections.reverse(list); // 反转
+        System.out.println("reverse " + list);
+        Collections.shuffle(list); // 打乱顺序
+        System.out.println("shuffle " + list);
+        Collections.sort(list);
+        System.out.println("sort " + list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println("sort " + list);
+        Collections.swap(list, 0, 2);
+        System.out.println("swap " + list);
+        Collections.rotate(list, 1);
+        System.out.println("rotate " + list);
+        System.out.println("binarySearch " + Collections.binarySearch(list, 1));
+        System.out.println("max " + Collections.max(list));
+        Collections.fill(list, 1);
+        System.out.println("fill " + list);
+        System.out.println("frequency " + Collections.frequency(list, 1));
+        Collections.replaceAll(list, 1, 2);
+        System.out.println("repalceAll " + list);
 
     }
 }
