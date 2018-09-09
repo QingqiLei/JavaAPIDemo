4e4c39b0dcd8c81b49fd7591103d597f5f5b277a
bomb <qingqi_lei@icloud.com>
2018-09-09 14:38:46 +0800

    FileInputStream and FileOutputStream

--- API/src/IOFile/ByteStreamDemo.java
+++ API/src/IOFile/ByteStreamDemo.java
@@ -6,9 +6,9 @@ import java.io.*;
 public class ByteStreamDemo {
     private static void out() throws IOException {
         File file = new File("1.txt");
-        if(!file.exists()) file.createNewFile();
+        if (!file.exists()) file.createNewFile();
         // true means appending the stream content to the tail
-        OutputStream out = new FileOutputStream(file,true);
+        OutputStream out = new FileOutputStream(file, true);
         String info = "soy milk\r\n";
         out.write(info.getBytes());
         out.write(info.getBytes());
@@ -16,14 +16,14 @@ public class ByteStreamDemo {
 
     }
 
-    private static void in() throws IOException{
+    private static void in() throws IOException {
         File file = new File("1.txt");
         InputStream in = new FileInputStream(file);
         byte[] bytes = new byte[10];                   // first
         StringBuilder buf = new StringBuilder();
-        int len = -1;
-        while((len = in.read(bytes))!= -1){           // second
-            buf.append(new String(bytes,0,len));      // third
+        int len;
+        while ((len = in.read(bytes)) != -1) {           // second
+            buf.append(new String(bytes, 0, len));      // third
 
         }
         in.close();
