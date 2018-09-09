997016c534ae979abe66c864a985db020cc4439a
bomb <qingqi_lei@icloud.com>
2018-09-09 10:32:59 +0800

    Collections class

--- API/src/container/CollectionDemo.java
+++ API/src/container/CollectionDemo.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class CollectionDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(1);
@@ -14,6 +14,7 @@ public class CollectionDemo {
         list.add(3);
         Collections.reverse(list);
         System.out.println(list);
+
 
     }
 }
