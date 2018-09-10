69ea668c6a021da4a393211474e6685cb0cf8cc8
bomb <qingqi_lei@icloud.com>
2018-09-10 16:16:36 +0800

    ByteArrayInputStream and ByteArrayOutputStream

--- API/src/IOFile/ByteArrayStreamD.java
+++ API/src/IOFile/ByteArrayStreamD.java
@@ -3,6 +3,9 @@ package IOFile;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
+/**
+ * base on memory, not on disk, in the memory, there is a byte Array that deal with stream
+ */
 public class ByteArrayStreamD {
     private static void byteArray() {
         String s = "123456dfafe";
