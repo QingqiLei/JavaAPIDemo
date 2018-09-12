d77b33d6a491898304e5b5839ab2fba2bb43b014
bomb <qingqi_lei@icloud.com>
2018-09-12 08:22:36 +0800

    Node

---
+++ API/src/generic/Node.java
@@ -0,0 +1,18 @@
+package generic;
+
+public class Node<T> {
+    private final T data;
+
+
+
+    public Node(T  data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+
+
+}
