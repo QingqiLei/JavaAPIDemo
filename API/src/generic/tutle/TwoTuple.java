748809cef0fa752222150328e5b412ec5e600fc4
bomb <qingqi_lei@icloud.com>
2018-10-13 13:25:52 +0800

    a Tuple of two elements

---
+++ API/src/generic/tutle/TwoTuple.java
@@ -0,0 +1,20 @@
+package generic.tutle;
+
+public class TwoTuple<A, B> {
+    public final A first;
+    public final B second;
+
+    public TwoTuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "TwoTuple{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
+
