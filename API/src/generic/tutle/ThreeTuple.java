15f81e0e1e6faa51608a8550bc617e65cb7fe27c
bomb <qingqi_lei@icloud.com>
2018-10-13 13:32:30 +0800

    ThreeTuple extends TwoTuple

---
+++ API/src/generic/tutle/ThreeTuple.java
@@ -0,0 +1,24 @@
+package generic.tutle;
+
+public class ThreeTuple<A, B, C> extends TwoTuple {
+    public final C third;
+
+    public ThreeTuple(A first, B second, C third) {
+        super(first, second);
+        this.third = third;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreeTuple{" +
+                "third=" + third +
+                ", first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        ThreeTuple<Integer, Integer, Integer> t = new ThreeTuple<>(1, 2, 3);
+        System.out.println(t.first);
+    }
+}
