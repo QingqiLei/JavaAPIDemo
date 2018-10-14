d6fad67f68e6b444529f1deb09a7f99bc34106fb
bomb <qingqi_lei@icloud.com>
2018-10-14 11:57:25 +0800

    Shelf consisted of product

---
+++ API/src/generic/innerclass/Shelf.java
@@ -0,0 +1,11 @@
+package generic.innerclass;
+
+import generic.genericinterface.Generators;
+
+import java.util.ArrayList;
+
+public class Shelf extends ArrayList<Product> {
+    public Shelf(int nProducts) {
+        Generators.fill(this, Product.generator, nProducts);
+    }
+}
