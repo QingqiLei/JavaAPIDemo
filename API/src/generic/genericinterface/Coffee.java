843469d70181bb7e2479d757d36394cc8c990914
bomb <qingqi_lei@icloud.com>
2018-10-14 10:56:06 +0800

    Coffee class

---
+++ API/src/generic/genericinterface/Coffee.java
@@ -0,0 +1,9 @@
+package generic.genericinterface;
+
+public class Coffee {
+    private static long counter = 0;
+    private final long id = counter++;
+    public String toString(){
+        return getClass().getSimpleName()+" "+id;
+    }
+}
