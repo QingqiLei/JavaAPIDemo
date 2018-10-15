b07ceeb759c3c6c5cb960574a86efba09006faec
bomb <qingqi_lei@icloud.com>
2018-10-15 20:23:56 +0800

    FileOutputStream FileInputStream RandomAccessFile getChannel

---
+++ API/src/IOFile/thinkinjava/GetChannel.java
@@ -0,0 +1,39 @@
+package IOFile.thinkinjava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * FileOutputStream
+ * RandomAccessFile
+ * FileInputStream
+ */
+public class GetChannel {
+    private static final int BSIZE = 1024;
+    public static void main(String[] args) throws Exception{
+
+        FileChannel fc = new FileOutputStream("redirect.txt").getChannel();
+        fc.write(ByteBuffer.wrap("some text".getBytes()));
+        fc.close();
+
+        fc = new RandomAccessFile("data.txt", "rw").getChannel();
+        fc.position(fc.size());  // move to the end(the start of the line)
+
+        fc.write(ByteBuffer.wrap("gg".getBytes()));
+        fc.close();
+        // read the file
+
+        fc = new FileInputStream("data.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocateDirect(BSIZE);
+        fc.read(buff); // 调用read 告知FileChannel 向ByteBuffer 存储字节，
+
+        buff.flip();
+        while(buff.hasRemaining())
+            System.out.print((char)buff.get());
+
+    }
+}
